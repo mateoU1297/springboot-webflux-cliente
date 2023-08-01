6d38262f223132ed7a596132249298ba9c0ba91e
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-08-01 15:42:14 -0500

    se crea repositorio

---
+++ src/main/java/com/udemy/springboot/webflux/cliente/app/SpringbootWebfluxClienteApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.webflux.cliente.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/springboot/webflux/cliente/app/SpringbootWebfluxClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.webflux.cliente.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
